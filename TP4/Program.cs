dd7e361270084e1e1f4a38d9a6ef7c2731e93f3f
Amaury Caballero <amcaballero2023@gmail.com>
2021-03-19 21:36:06 -0400

    Done

--- TP4/Program.cs
+++ TP4/Program.cs
@@ -1,7 +1,7 @@
-﻿
-using System;
+﻿using System;
 using System.Collections.Generic;
 using static System.Console;
+using Microsoft.VisualBasic;
 
 namespace TP4
 {
@@ -18,11 +18,11 @@ namespace TP4
             Write("Original queue:\t");
             PrintQueue(queue);
 
+
             string reverse ="";
             foreach (var element in queue)
             {
                 reverse = $"{element}|{reverse}";
-                //Write($"{reverse}{queue.Dequeue()}");
             }
             queue.Clear();
 
@@ -44,6 +44,62 @@ namespace TP4
 
             Write("\nTwist queue:\t");
             PrintQueue(queue);
+
+
+            
+
+            string values = "321*+9-";
+            WriteLine($"\n\nThe characters are: {values}");
+            Stack<int> stack = new Stack<int>();
+
+            foreach (char item in values)
+            {
+                if (Information.IsNumeric(item))
+                {
+                    stack.Push(Convert.ToInt16(item.ToString()));
+                }   
+                else
+                {
+                    int num2 = stack.Pop();
+                    int num1 =  stack.Pop();
+
+                    num1 = Calculator(num1,num2,item.ToString());
+                    stack.Push(num1);
+
+                }
+            }
+
+            WriteLine($"Output: {stack.Pop()}");
+
+        }
+        static void PrintStack(Stack<int> stack)
+        {
+            foreach (var item in stack)
+            {
+                Write($"{item} - ");   
+            }
+        }
+        public static int Calculator(int num1, int num2, string symbol)
+        { 
+                if (symbol == "+")
+                {
+                    return num1 + num2;
+                }
+                else if (symbol == "-" )
+                {
+                    return num1 - num2;
+                }
+                else if (symbol == "*")
+                {
+                    return num1 * num2;
+                }
+                else 
+                {
+                    return num1 / num2;
+                }
+    
+            
+            
         }
         static void PrintQueue(Queue<string> queue)
         {
